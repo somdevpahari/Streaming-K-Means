bf9a66955a5d4916c709cc49db393de974cf97ed
Somdev Pahari <somdevpahari1@gmail.com>
2021-06-04 17:21:47 +0530

    Add files via upload

---
+++ Utilities.scala
@@ -0,0 +1,31 @@
+package com.somdev.sparkstreaming
+
+import org.apache.log4j.Level
+import java.util.regex.Pattern
+import java.util.regex.Matcher
+
+object Utilities {
+    /** Makes sure only ERROR messages get logged to avoid log spam. */
+  def setupLogging() = {
+    import org.apache.log4j.{Level, Logger}   
+    val rootLogger = Logger.getRootLogger()
+    rootLogger.setLevel(Level.ERROR)   
+  }
+  
+  
+  /** Retrieves a regex Pattern for parsing Apache access logs. */
+  def apacheLogPattern():Pattern = {
+    val ddd = "\\d{1,3}"                      
+    val ip = s"($ddd\\.$ddd\\.$ddd\\.$ddd)?"  
+    val client = "(\\S+)"                     
+    val user = "(\\S+)"
+    val dateTime = "(\\[.+?\\])"              
+    val request = "\"(.*?)\""                 
+    val status = "(\\d{3})"
+    val bytes = "(\\S+)"                     
+    val referer = "\"(.*?)\""
+    val agent = "\"(.*?)\""
+    val regex = s"$ip $client $user $dateTime $request $status $bytes $referer $agent"
+    Pattern.compile(regex)    
+  }
+}
